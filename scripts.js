f4e74576993f1ce983b56d3338712fb0da672b2b
kuga-uy <eduardodiaz250196@gmail.com>
2021-10-28 18:49:21 -0300

    greetings and weather container

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const greetingText = document.querySelector('#variable-greetings');
+const currentDate = new Date();
+
+const hours = currentDate.getHours();
+
+const greetingsFunction = () =>{
+    
+        if ( hours <= 12 ) {
+            greetingText.textContent = "Good Morning! ";
+        } else if ( hours > 12 ) {
+            greetingText.textContent = "Good Afternoon! ";
+        } else if (hours > 18) {
+            greetingText.textContent = "Good Evening! ";
+        } else if ( hours > 20 ) {
+            greetingText.textContent = "Good Night! ";
+        }
+            
+    }      
+       
+
+window.addEventListener('DOMContentLoaded', greetingsFunction());
