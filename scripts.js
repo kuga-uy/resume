959d394fc1c3fcca80b31bfa8ea948f0ef76de32
kuga-uy <eduardodiaz250196@gmail.com>
2021-10-28 21:11:42 -0300

    fixing logic

--- scripts.js
+++ scripts.js
@@ -7,9 +7,9 @@ const greetingsFunction = () =>{
     
         if ( hours <= 12 ) {
             greetingText.textContent = "Good Morning! ";
-        } else if ( hours > 12 ) {
+        } else if ( hours > 12 && hours < 18 ) {
             greetingText.textContent = "Good Afternoon! ";
-        } else if (hours > 18) {
+        } else if (hours > 18 && hours < 20) {
             greetingText.textContent = "Good Evening! ";
         } else if ( hours > 20 ) {
             greetingText.textContent = "Good Night! ";
